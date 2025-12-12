4705e129375c37bff97c987a491b0c21e21b3f1e
Ashutosh4192 <123867114+Ashutosh4192@users.noreply.github.com>
2025-12-12 19:14:21 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Diversifi - Mini Portfolio',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: Text("Home"),
+    );
+  }
+}
